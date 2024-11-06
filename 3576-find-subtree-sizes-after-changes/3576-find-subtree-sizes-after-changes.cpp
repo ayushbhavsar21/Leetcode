69ba858703d555e0d14497e78ec700a3beba3c04
Ayush Bhavsar <141308134+ayushbhavsar21@users.noreply.github.com>
2024-11-06 11:36:00 +0530

    Time: 339 ms (53.06%), Space: 280.7 MB (20.03%) - LeetHub

--- 3576-find-subtree-sizes-after-changes/3576-find-subtree-sizes-after-changes.cpp
+++ 3576-find-subtree-sizes-after-changes/3576-find-subtree-sizes-after-changes.cpp
@@ -1,35 +1,55 @@
 class Solution {
 public:
-    int dfs(int i,vector<vector<int>> &adj,vector<int> &ans){
-        int s = 1;
-        for(auto it:adj[i]){
-            s += dfs(it,adj,ans);
-        }
-        ans[i] = s;
-        return s;
-    }
-    vector<int> findSubtreeSizes(vector<int>& p, string s) {
-        vector<int> p2  = p;
-        int n = p.size();
+    void change(int i, vector<int>& closean, vector<int>& newp, vector<vector<int>>& adj, string& s) {
+        int c = s[i] - 'a';
         
-        for(int i=1;i<n;i++){
-            int y = p[i];
-
-            while(y != -1 && s[y] != s[i]){
-                y = p[y];
-            }
-            if(y != -1){
-                p2[i] = y;
-            }
+        
+        if(closean[c] != -1) {
+            newp[i] = closean[c];
         }
-
-        vector<vector<int>> adj (n);
-        for(int i=1;i<n;i++){
-            adj[p2[i]].push_back(i);
+        
+        int oldan = closean[c];
+        closean[c] = i;
+        
+        for(auto it : adj[i]) {
+            change(it, closean, newp, adj, s);
         }
-        vector<int> ans(n,0);
-        dfs(0,adj,ans);
+        
+        closean[c] = oldan;
+    }
+    
+    int dfs(int i, vector<vector<int>>& newadj, vector<int>& ans) {
+        int size = 1;
+        
+        for(auto it : newadj[i]) {
+            size += dfs(it, newadj, ans);
+        }
+        return ans[i] = size;
+    }
+    
+    vector<int> findSubtreeSizes(vector<int>& p, string s) {
+        int n = p.size();
+        vector<int> closean(26, -1);
+        
+        vector<int> newp = p;
+        vector<vector<int>> adj(n);
+        vector<vector<int>> newadj(n);
+        
+        
+        for(int i = 1; i < n; i++) {
+            adj[p[i]].push_back(i);
+        }
+        
+        change(0, closean, newp, adj, s);
+        
+        
+        for(int i = 1; i < n; i++) {
+            newadj[newp[i]].push_back(i);
+        }
+        
+        vector<int> ans(n, 0);
+        dfs(0, newadj, ans);
+        
         return ans;
-
     }
 };
