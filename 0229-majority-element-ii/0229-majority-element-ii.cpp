54ba389d48d01e663a536b81a4db8324307b5456
Ayush Bhavsar <141308134+ayushbhavsar21@users.noreply.github.com>
2025-03-02 12:58:41 +0530

    Time: 0 ms (100%), Space: 19.7 MB (97.63%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,18 +1,42 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
+        int x1=0,x2=0,c1=0,c2=0;
         int n = nums.size();
         int l = n/3;
-        // int maxi = *max_element(nums.begin(),nums.end());
-        // int mini = *min_element(nums.begin(),nums.end());
-        vector<int> ans ;
-        unordered_map<int,int> mp ;
-        for(auto it:nums)
-            mp[it]++;
-        for(auto it:mp){
-            if(it.second > l)
-                ans.push_back(it.first);
+
+        for(auto it:nums){
+            if(c1 == 0 && x2 != it){
+                x1 = it;
+                c1++;
+            }
+            else if(c2 == 0 && x1 != it){
+                x2 = it;
+                c2++;
+            }
+            else if(x1 == it)
+                c1++;
+            else if(x2 == it)
+                c2++;
+            else{
+                c1--;
+                c2--;
+            }
         }
+        c1 =0,c2=0;
+        vector<int> ans;
+        for(auto it:nums){
+            if(x1 == it)
+                c1++;
+            else if(x2 == it)
+                c2++;
+        }
+
+        if(c1 > l)
+            ans.push_back(x1);
+        if(c2 > l)
+            ans.push_back(x2);
         return ans;
+
     }
 };
