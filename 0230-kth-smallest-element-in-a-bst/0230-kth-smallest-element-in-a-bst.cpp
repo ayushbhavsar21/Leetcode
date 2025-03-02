0afac68596e0b5a793abc4bdb6c4760fd7ff2a81
Ayush Bhavsar <141308134+ayushbhavsar21@users.noreply.github.com>
2025-03-02 19:02:52 +0530

    Time: 0 ms (100%), Space: 24.5 MB (28.68%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -1,35 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    
-    int dfs(TreeNode* root, unordered_map<TreeNode*, int>& mp) {
-        if (!root) return 0; 
-        
-        int l = dfs(root->left, mp);  
-        int r = dfs(root->right, mp); 
-        
-        mp[root] = l + r + 1; 
-        return mp[root];
-    }
+    void inorder(TreeNode* root,vector<int>&v){
+        if(!root)
+            return;
 
-    
+        inorder(root->left,v);
+        v.push_back(root->val);
+        inorder(root->right,v);
+    }
     int kthSmallest(TreeNode* root, int k) {
-        unordered_map<TreeNode*, int> mp;
-        dfs(root, mp); 
-        
-        TreeNode* curr = root;
-        while (curr) {
-            int leftSize = (curr->left ? mp[curr->left] : 0);
-            
-            if (leftSize + 1 == k) {
-                return curr->val; 
-            } else if (leftSize + 1 < k) {
-                k -= (leftSize + 1); 
-                curr = curr->right;
-            } else {
-                curr = curr->left; 
-            }
-        }
-        
-        return -1;
+        vector<int> v;
+        inorder(root,v);
+        return v[k-1];
     }
 };
