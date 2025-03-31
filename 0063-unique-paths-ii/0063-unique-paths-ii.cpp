93caf3241dd4cb1570987a464dffa6a8b5277b0b
Ayush Bhavsar <141308134+ayushbhavsar21@users.noreply.github.com>
2025-03-31 12:50:36 +0530

    Time: 0 ms (100%), Space: 12 MB (28.53%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -3,22 +3,27 @@ public:
     int uniquePathsWithObstacles(vector<vector<int>>& v) {
         int n = v.size();
         int m = v[0].size();
-
-        if(v[n-1][m-1] == 1 || v[0][0] == 1)
+        if(v[0][0] == 1 || v[n-1][m-1] == 1)
             return 0;
-        vector<vector<int>> dp (n + 1 , vector<int>(m +1 , 0));
+        if(n== 1 && m == 1)
+            return 1;
+
+        vector<vector<int>> dp(n,vector<int>(m,0));
         dp[0][0] = 1;
-
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(i == 0 && j==0) continue;
-
-                if(i-1 >= 0 && v[i][j] == 0 ){
-                    dp[i][j] += dp[i-1][j] ;
-                }
-                if(j-1 >= 0 && v[i][j] == 0 ){
-                    dp[i][j] += dp[i][j-1] ;
-                }             
+        for(int i=1;i<n;i++){
+            if(v[i][0] == 1)
+                break;
+            dp[i][0] = 1;
+        }
+        for(int i=1;i<m;i++){
+            if(v[0][i] == 1)
+                break;
+            dp[0][i] = 1;
+        }
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){                
+                if(v[i][j] == 0)
+                    dp[i][j] += dp[i-1][j] + dp[i][j-1];
             }
         }
         return dp[n-1][m-1];
