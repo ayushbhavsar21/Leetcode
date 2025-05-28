d1a34f11e3a23754248f867e276494c4178fe852
Ayush Bhavsar <141308134+ayushbhavsar21@users.noreply.github.com>
2025-05-28 19:33:26 +0530

    Time: 382 ms (81.89%), Space: 92.2 MB (84.16%) - LeetHub

--- 0282-expression-add-operators/0282-expression-add-operators.cpp
+++ 0282-expression-add-operators/0282-expression-add-operators.cpp
@@ -1,32 +1,37 @@
 class Solution {
 public:
-    vector<string> ans;
     string s;
-    int target;
-    vector<string> addOperators(string s, int target) {
-        this->s = s;
-        this->target = target;
-        backtrack( 0, "", 0, 0);
-        return ans;
-    }
-    void backtrack(int i, const string& path, long resSoFar, long prevNum) {
-        if (i == s.length()) {
-            if (resSoFar == target) ans.push_back(path);
-            return;
+    int t;
+    void solve(int i,string path,long sum,long prev,vector<string>& ans){
+        if(i == s.length()){
+            if(sum == t)
+                ans.push_back(path);
+            return ;
         }
-        string numStr;
-        long num = 0;
-        for (int j = i; j < s.length(); j++) {
-            if (j > i && s[i] == '0') break; // Skip leading zero number
-            numStr += s[j];
-            num = num * 10 + s[j] - '0';
-            if (i == 0) {
-                backtrack(j + 1, path + numStr, num, num); // First num, pick it without adding any operator!
-            } else {
-                backtrack(j + 1, path + "+" + numStr, resSoFar + num, num);
-                backtrack(j + 1, path + "-" + numStr, resSoFar - num, -num);
-                backtrack(j + 1, path + "*" + numStr, resSoFar - prevNum + prevNum * num, prevNum * num); // Can imagine with example: 1-2*3
+        string s2 = "";
+        long sum2 = 0;
+
+        for(int j=i;j<s.length();j++){
+            if(j > i && s[i] == '0') break;
+
+            s2 += s[j];
+            sum2 = sum2*10 + (s[j] - '0');
+
+            if(i == 0)
+                solve(j+1, s2 , sum2 , sum2 , ans);
+            else{
+                solve(j+1,path +"+" + s2 , sum + sum2 , sum2 , ans);
+                solve(j+1,path +"-" + s2 , sum - sum2 , -sum2 , ans);
+                solve(j+1,path +"*" + s2 , sum - prev + prev * sum2 ,prev*sum2 , ans);
+
             }
         }
     }
-};
+    vector<string> addOperators(string num, int target) {
+        s = num;
+        t = target;
+        vector<string> ans;
+        solve(0,"",0,0,ans);
+        return ans;
+    }
+};  
