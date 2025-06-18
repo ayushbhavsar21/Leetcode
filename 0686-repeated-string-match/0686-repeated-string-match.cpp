bfeb137ea835769e45ee98f82ae6396f38ca2fca
Ayush Bhavsar <141308134+ayushbhavsar21@users.noreply.github.com>
2025-06-18 20:53:44 +0530

    Time: 0 ms (100%), Space: 11.2 MB (19.96%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.cpp
+++ 0686-repeated-string-match/0686-repeated-string-match.cpp
@@ -1,45 +1,54 @@
 class Solution {
-private:
-    int BASE = 100000;
 public:
-    int repeatedStringMatch(string A, string B) {
-        if(A == B) return 1;
-        int count = 1;
-        string source = A;
-        while(source.size() < B.size()){
-            count++;
-            source+=A;
+    vector<int> prefix(string b){
+        int n = b.length();
+        vector<int> v(n,0);
+        int j = 0;
+        for(int i=1;i<n;i++){
+            while(j>0 && b[i] != b[j])
+                j = v[j-1];
+            if(b[i] == b[j])
+                j++;
+            v[i] = j;
         }
-        if(source == B) return count;
-        if(Rabin_Karp(source,B) != -1) return count;
-        if(Rabin_Karp(source+A,B) != -1) return count+1;
-        return -1;
+        return v;
     }
-    int Rabin_Karp(string source, string target){
-        if(source == "" or target == "") return -1;
-        int m = target.size();
-        int power = 1;
-        for(int i = 0;i<m;i++){
-            power = (power*31)%BASE;
-        }
-        int targetCode = 0;
-        for(int i = 0;i<m;i++){
-            targetCode = (targetCode*31+target[i])%BASE;
-        }
-        int hashCode = 0;
-        for(int i = 0;i<source.size();i++){
-            hashCode = (hashCode*31 + source[i])%BASE;
-            if(i<m-1) continue;
-            if(i>=m){
-                hashCode = (hashCode-source[i-m]*power)%BASE;
+    bool kmp(string s, string b){
+        int n = s.length() , m = b.length();
+        vector<int> v = prefix(b);
+        int i =0 , j =0;
+        while(i<n){
+            if(s[i] == b[j]){
+                i++;
+                j++;
             }
-            if(hashCode<0)
-                hashCode+=BASE;
-            if(hashCode == targetCode){
-                if(source.substr(i-m+1,m) == target)
-                    return i-m+1;
+            else{
+                if(j!= 0)
+                    j = v[j-1];
+                else
+                    i++;
             }
+            if(j == m)
+                return 1;
         }
+        return 0;
+    }
+    int repeatedStringMatch(string a, string b) {
+        string s = a;
+        int count = 1;
+
+        while(s.length() < b.length()){
+            s += a;
+            count++;
+        }
+
+        if(kmp(s,b)) return count;
+
+        s += a;
+        count++;
+
+        if(kmp(s,b)) return count;
+
         return -1;
     }
 };
