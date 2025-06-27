3750e21faba661c66d9678858cef392045710a34
Ayush Bhavsar <141308134+ayushbhavsar21@users.noreply.github.com>
2025-06-27 18:59:26 +0530

    Time: 276 ms (55.92%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,5 +1,14 @@
-select
-(select distinct Salary 
-from Employee order by salary desc 
-limit 1 offset 1) 
-as SecondHighestSalary;
+# Write your MySQL query statement below
+select distinct salary as SecondHighestSalary 
+from Employee 
+union all
+
+select null as SecondHighestSalary
+union all
+
+select null as SecondHighestSalary
+
+
+order by SecondHighestSalary desc
+limit 1
+offset 1;
